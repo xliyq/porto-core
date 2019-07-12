1b3729dfdcc1ec7996fa08fd00da20fc409416f3
liyq <2847895875@qq.com>
2019-07-12 16:20:08 +0800

    TestCase不在实现createApplication

--- Tests/PhpUnit/CoreTestCase.php
+++ Tests/PhpUnit/CoreTestCase.php
@@ -49,21 +49,6 @@ abstract class CoreTestCase extends LaravelTestCase
         parent::tearDown();
     }
 
-    public function createApplication() {
-        $this->baseUrl = env('API_FULL_URL');
-
-        $this->overrideSubDomain();
-
-        $app = require __DIR__ . '/../../../../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        $this->faker = $app->make(Generator::class);
-
-        return $app;
-    }
-
-
     /**
      * 刷新内存中的数据库
      */
